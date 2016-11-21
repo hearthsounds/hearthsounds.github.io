4e231c218848750ddcce4f1125fe7a40c3b591ce
Blake <briosa@atlassian.com>
2016-11-22 09:46:49 +1100

    card data to js

---
+++ data-generator/card-data-to-js.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+cards = []
+DIR = 'card-data'
+
+for filename in os.listdir(DIR):
+  with open(DIR+'/'+filename) as f:
+    card = json.load(f)
+    cards.append({'name': card['name'], 'sound': card['sounds']})
+
+with open('output.json', 'w') as f:
+  json.dump(cards, f)
