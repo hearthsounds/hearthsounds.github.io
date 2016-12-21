a9ed8b4a2230a4ecbf8af102e47f14536d2cf1c3
rissole <bonzaibrother@gmail.com>
2016-12-21 17:50:02 +1100

    update card to js generation

--- data-generator/card-data-to-js.py
+++ data-generator/card-data-to-js.py
@@ -7,7 +7,7 @@ DIR = 'card-data'
 for filename in os.listdir(DIR):
   with open(DIR+'/'+filename) as f:
     card = json.load(f)
-    cards.append({'name': card['name'], 'sound': card['sounds']})
+    cards.append({'name': card['name'], 'sound': card['sounds'], 'img': card['img']})
 
 with open('output.json', 'w') as f:
   json.dump(cards, f)
